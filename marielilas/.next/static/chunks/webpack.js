455c5442ab9efbfae66c9267d8fc8bf767977eda
MathieuFigari <mathieu.figari@gmail.com>
2022-07-08 00:14:35 +0200

    responsive and navbar ok

--- marielilas/.next/static/chunks/webpack.js
+++ marielilas/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c19d577b33d4c895"; }
+/******/ 		__webpack_require__.h = function() { return "7d11a05d71ea8ea5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- marielilas/components/NavBar/index.js
+++ marielilas/components/NavBar/index.js
@@ -3,9 +3,13 @@ import Link from 'next/link'
 import styles from './Navbar.module.css'
 import Image from 'next/image'
 import logo from '../../public/assets/logo.png'
+import { useRouter } from "next/router"
 
 
 export default function NavBar() {
+
+const router = useRouter()
+
   return (
     <header className={styles.header}>
       <div className={styles.siteHeader}>
@@ -17,9 +21,15 @@ export default function NavBar() {
     width="100" height="100"
     />
     </div>
+    <div className={router.asPath == "/" ? styles["link_active"] :  styles["link"]}>
       <Link href="/"><a>Accueil</a></Link>
+      </div>
+      <div className={router.pathname == "/Articles" || router.pathname == "/Articles/[article]" ? styles["link_active"] :  styles["link"]}>
       <Link href="/Articles"><a>Articles</a></Link>
+      </div>
+      <div className={router.asPath == "/#scrollToContact" ? styles["link_active"] :  styles["link"]}>
       <Link href="/#scrollToContact"><a>Contact</a></Link>
+      </div>
     </nav>
     </div>
     </header>
